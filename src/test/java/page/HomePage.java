2ab4ca5fa3d2f0927205bcb49659124b3e656e70
sumanthkbs <sumanthsumikbs@gmail.com>
2025-02-06 16:46:08 +0530

    POM class for homepage

---
+++ src/test/java/page/HomePage.java
@@ -0,0 +1,33 @@
+package page;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Reporter;
+
+public class HomePage {
+
+	@FindBy(xpath="//h3[starts-with(text(),'Welcome to OSPOS')]")
+	private WebElement welcomeMessage;
+	
+	public HomePage(WebDriver driver)
+	{
+		PageFactory.initElements(driver,this);
+	}
+	public boolean verifyHomePageIsDisplayed(WebDriverWait wait)
+	{
+		try {
+	 wait.until(ExpectedConditions.visibilityOf(welcomeMessage));
+		Reporter.log("HOme page is displayed",true);
+		return true;
+	}
+		catch(Exception e)
+		{
+			Reporter.log("HOme page is displayed",true);
+          return false;
+		}
+}
+}

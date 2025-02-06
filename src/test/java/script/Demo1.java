fbf12911a40653dab357bdcc72453a63a28ad1b5
sumanthkbs <sumanthsumikbs@gmail.com>
2025-02-06 11:36:24 +0530

    New Automation script

--- src/test/java/script/Demo1.java
+++ src/test/java/script/Demo1.java
@@ -1,25 +1,5 @@
 package script;
 
-import org.testng.Assert;
+public class Demo1 {
 
-import org.testng.Reporter;
-import org.testng.annotations.Test;
-
-import generic.BaseTest;
-import generic.Utility;
-
-public class Demo1 extends BaseTest{
-
-	@Test
-	public void testC()
-	{
-		String data = Utility.getXLCellData(XL_PATH,"sheet1", 0, 0);
-		test.info("data from excel"+data);
-		
-		String title = driver.getTitle();
-		
-		test.info("testC"+title);
-		
-	}
 }
-
